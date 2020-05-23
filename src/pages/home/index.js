70623e6a6fd019e255b85dc2ee8d069aa5a9e987
EleneKikalishvili <elene.kikalishvili.1@btu.edu.ge>
2020-05-23 05:23:42 +0400

    fixed redirect issue

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,19 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useContext } from "react";
+import { AuthContext } from "../../context/authContext";
 import { Redirect } from "react-router-dom";
-
+import UserList from "./users/UserList";
 function Home() {
   const [redirect, setRedirect] = useState(false);
-  function handleClick() {
-    localStorage.removeItem("Token");
-    setRedirect(true);
-  }
+  const auth = useContext(AuthContext);
 
-  if (redirect) {
-    return <Redirect to="/login" />;
-  }
+  const handleClick = () => {
+    localStorage.removeItem("Token");
+    auth.login();
+    setRedirect(true);
+  };
+
+  // function handleClick() {
+  //   localStorage.removeItem("Token");
+  //   setRedirect(true);
+  // }
 
   return (
     <div>
+      {redirect && <Redirect to="/login" />}
       <h3>Home</h3>
       <button
         onClick={() => {
