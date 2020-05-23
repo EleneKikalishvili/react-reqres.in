11b9bc612944f1273cc75fcff925684decdf6f6c
EleneKikalishvili <elene.kikalishvili.1@btu.edu.ge>
2020-05-23 04:41:16 +0400

    changes in call method

--- src/api/index.js
+++ src/api/index.js
@@ -2,19 +2,22 @@ import axios from "axios";
 
 axios.defaults.baseURL = "https://reqres.in/api";
 
-export const makeGetReq = (url) =>
-  axios
-    .get(url)
-    .then((res) => res)
-    .catch((error) => {
-      throw error;
-    });
+export const makeGetReq = async (url) => {
+  try {
+    const response = await axios.get(url);
+    console.log(response);
+    return response;
+  } catch (error) {
+    throw error;
+  }
+};
+
 export const makePostReq = async (params) => {
   try {
     const response = await axios.post(params.url, params.data);
     return response;
   } catch (error) {
-    window.alert("user doesn't exist");
+    window.alert("can't use this email");
     throw error;
   }
 };
