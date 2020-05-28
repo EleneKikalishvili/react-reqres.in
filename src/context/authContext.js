19688cb07a0e8b82ea9402c527f96ac34e455338
EleneKikalishvili <elene.kikalishvili.1@btu.edu.ge>
2020-05-28 22:55:23 +0400

    added logoutHandler, setting token to localStorage in loginHandler

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -3,15 +3,24 @@ import React, { useState } from "react";
 export const AuthContext = React.createContext({
   isAuth: false,
   login: () => {},
+  logout: () => {},
 });
 
 function AuthComponent(props) {
   const [isAuth, setIsAuth] = useState();
-  const loginHandler = () => {
-    setIsAuth(!isAuth);
+  const loginHandler = (res) => {
+    localStorage.setItem("Token", res.data.token);
+    setIsAuth(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem("Token");
+    setIsAuth(false);
   };
   return (
-    <AuthContext.Provider value={{ isAuth: isAuth, login: loginHandler }}>
+    <AuthContext.Provider
+      value={{ isAuth: isAuth, login: loginHandler, logout: logoutHandler }}
+    >
       {props.children}
     </AuthContext.Provider>
   );
